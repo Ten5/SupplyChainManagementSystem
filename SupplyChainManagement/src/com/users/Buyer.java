1e0612d85a4eece15a392b5cc6620db09cb6b17a
SubhayuChakravorty <sbhchakravorty@gmail.com>
2016-11-30 20:09:11 -0500

    Removed Comment.

--- SupplyChainManagement/src/com/users/Buyer.java
+++ SupplyChainManagement/src/com/users/Buyer.java
@@ -6,7 +6,7 @@ public class Buyer extends User implements MakeDecision {
 	public Buyer(String name, String password, String email) {
 		super(name, password, email);
 	}
-	//Hi I have added a comment
+	
 	@Override
 	public void login() {
 		// TODO Auto-generated method stub
